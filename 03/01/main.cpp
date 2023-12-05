0f918a5cfa939bde0145816a037144e600bd166b
nikpan1 <nikodem.panknin@gmail.com>
2023-12-05 14:37:28 +0100

    day 3 part 1

--- 03/01/main.cpp
+++ 03/01/main.cpp
@@ -3,73 +3,72 @@
 #include <string>
 #include <vector>
 
-
+#define BUFFER_SIZE 3
+#define EMPTY_CHAR '.'
 bool isNumber(char c) { return (c <= 57) && (c >= 48); }
-bool isPart(char c) { return !isNumber(c) && c != '.'; }
+bool isPart(char c) { return !isNumber(c) && c != EMPTY_CHAR; }
 
 
-int getWholeNumber(std::string line, int &start) {
+int getWholeNumber(std::string& line, int start) {
   int end = start;
-
   while(end < line.size() && isNumber(line[end])) end ++;
   while(start >= 0 && isNumber(line[start])) start --;
   
-
   int result = std::stoi(line.substr(start + 1, end - start));
-  start = end + 1;
+  for(int i = start + 1; i <= end - 1; i ++) line[i] = EMPTY_CHAR;
+  
   return result;
 }
 
 
-// @TODO niektóre rzeczy liczy podwójnie, a niektóre wcale :/
-int findValue(std::vector<std::string> buffer, int cx, int cy) {
-  int x(0), y(0);
-  for(int i = -1 ; i < 1; i ++) {
-    for(int j = -1; j < 1; j ++) {
-      x = cx + j; y = cy + i;
-      if(x < 0 || y < 0 || x > buffer[0].size() || y > buffer.size()) continue;
-      else if(isNumber(buffer[y][x])) { int val = getWholeNumber(buffer[y],x); std::cout << "b"<<val<< "\n";return val;};
-    }
+int findPartNumber(std::vector<std::string>& buffer, int cx, int cy) {
+  int result(0);
+
+  for(int x = cx - 1 ; x <= cx + 1; x ++) 
+    for(int y = cy - 1; y <= cy + 1 ; y ++) 
+      if((x >= 0 && x < buffer.size()) && (y >= 0 && y < buffer[x].size()) && (isNumber(buffer[x][y])))
+        result += getWholeNumber(buffer[x], y);
+
+  return result;
+}
+
+
+int validateEngine(std::vector<std::string>& buffer) {
+  int result(0);
+  for(int x = 0; x < buffer.size(); x ++) 
+    for(int y = 0; y < buffer[x].size(); y ++) 
+      if(isPart(buffer[x][y])) result += findPartNumber(buffer, x, y);
+  
+  return result;
+}
+
+
+int main(int argc, char** argv) {
+  if(argc < 2) {
+    std::cerr << "Dej mi tu wincyj inputu.\n";
+    return -1;
   }
 
-  return 0;
-}
-
-
-int validateEngine(std::vector<std::string> buffer) {
-  int result(0);
-
-  for(int x = 0; x < buffer[0].size(); x ++) {
-    for(int y = 0; y < buffer.size(); y ++) {
-      if(isPart(buffer[y][x])) result += findValue(buffer, x, y);
-    }
-   }
-//  std::cout<<"r=" << result << "\n"; 
-  return result;
-}
-
-
-int main() {
-  int result(0);
-  
   std::ifstream input;
-  input.open("input.txt");
-  if(!input.is_open()) return -1;
-  
+  input.open(argv[1]);
+  if(!input.is_open()) {
+    std::cerr << "File failed to open.\n";
+    return -1;
+  }
 
+
+  int result(0);
   std::vector<std::string> buffer; 
   std::string line;
 
-  int counter(0);  
-  do {  
-    buffer.push_back(line);  
-  } while(counter ++ < 2 && std::getline(input, line));   // assumption: at least 3 lines in the file 
-  
-  while(std::getline(input, line)) {
-    result += validateEngine(buffer);
+  while(input) { 
+    while(BUFFER_SIZE > buffer.size()) {
+     std::getline(input, line);
+     buffer.push_back(line);
+    }
     
-    buffer.push_back(line);
-    buffer.erase(buffer.begin());
+    result += validateEngine(buffer);
+    for(int i = 1; i < BUFFER_SIZE; i ++) buffer.erase(buffer.begin());
   }
 
   std::cout << "result = " << result;
