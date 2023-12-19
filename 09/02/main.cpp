ca5f4a011b5dd47c67361db45807fee3af9d9129
nikpan1 <nikodem.panknin@gmail.com>
2023-12-19 23:58:14 +0100

    day 9 part 2

---
+++ 09/02/main.cpp
@@ -0,0 +1,69 @@
+#include <iostream>
+#include <fstream>
+#include <vector> 
+#include <string>
+#include <sstream>
+
+
+std::vector<int> split(const std::string &str, char separator = ' ') {
+  std::vector<int> result;
+  std::istringstream iss(str);
+  std::string value;
+
+  while(std::getline(iss, value, separator)) result.push_back(std::stoi(value));
+  return result;
+}
+
+
+int getFirstDist(std::vector<int>& his) {
+  std::vector<int> result;
+  for(int i = 0; i < his.size() - 1; i ++) result.push_back(his[i + 1] - his[i]);
+  his = result;
+  return his[0]; 
+}
+
+
+bool areDistEqual(const std::vector<int>& dists) {
+  if(dists.size() == 1) return true;
+  for(auto dist : dists) if(dist != 0) return false;
+  return true;
+}
+
+
+int getFirstVal(std::vector<int> history) {
+  std::vector<int> allFirstDists;
+  allFirstDists.push_back(history.front());
+
+  while(!areDistEqual(history)) allFirstDists.push_back(getFirstDist(history));
+  
+  allFirstDists.pop_back();
+  int result = allFirstDists.back();
+  allFirstDists.pop_back(); 
+
+  for(auto dist = allFirstDists.rbegin(); dist != allFirstDists.rend(); dist ++) result = *dist - result;
+
+  return result;
+}
+
+
+int main(int argc, char** argv) {
+  if(argc != 2) {
+    std::cerr << "Wrong amount of arguments.\n";
+    return -1;
+  }
+
+  std::ifstream input;
+  input.open(argv[1]);
+  if(!input.is_open()) {
+    std::cerr << "File not opened.\n";
+    return -1;
+  }
+  
+  int result(0);
+  std::string line;
+
+  while(std::getline(input, line)) result += getFirstVal(split(line));
+
+  std::cout << "result = " << result;
+  return result;
+}
